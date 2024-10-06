f2c55d9477854d322a630e2b75188b99e47c54b5
Femi Oshin <30566275+fmstyles14@users.noreply.github.com>
2024-10-06 19:08:31 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(jenkin-server-credentials : fmstyles/demo-app, passwordVariable: PASSWORD, usernameVariable: USERNAME)]) {
+    withCredentials([usernamePassword(credentials: 'jenkin-server-credentials', passwordVariable: PASSWORD, usernameVariable: USERNAME)]) {
         sh 'docker build -t fmstyles/demo-app:jma-2.0 .'
         sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
         sh 'docker push fmstyles/demo-app:jma-2.0'
